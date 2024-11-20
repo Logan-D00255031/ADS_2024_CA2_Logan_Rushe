dd77ec01ee4f1c79dbd28f1d64708298ecc28178
Logan Rushe <D00255031@student.dkit.ie>
2024-11-20 12:57:17 +0000

    🔑  Implemented TreeMap Methods

--- CA2/TreeMap.h
+++ CA2/TreeMap.h
@@ -66,7 +66,8 @@ template<class K, class V>
 bool TreeMap<K, V>::containsKey(K key)
 {
 	TreeMapNode<K, V> desiredKey = new TreeMapNode<K, V>(key);
-	if (BST->get(desiredKey))
+	TreeMapNode<K, V> foundKey = BST->get(desiredKey);
+	if (foundKey.getKey() == key)
 	{
 		return true;
 	}
@@ -76,40 +77,69 @@ bool TreeMap<K, V>::containsKey(K key)
 template<class K, class V>
 V& TreeMap<K, V>::get(K key)
 {
-	// TODO: insert return statement here
+	if (!containsKey(key))
+	{
+		return NULL;
+	}
+	TreeMapNode<K, V> foundKey = BST->get(new TreeMapNode<K, V>(key));
+	return foundKey.getValue();
 }
 
 template<class K, class V>
 BinaryTree<K> TreeMap<K, V>::keySet()
 {
-	return BinaryTree<K>();
+	TreeMapNode<K, V> *keyArr = BST->toArray();
+	BinaryTree<K> keyTree = new BinaryTree<K>;
+	for (TreeMapNode<K, V> currentNode : keyArr) 
+	{
+		keyTree.add(currentNode.getKey());
+	}
+	return keyTree;
 }
 
 template<class K, class V>
 void TreeMap<K, V>::put(K key, V value)
 {
+	if (!containsKey(key))
+	{
+		BST->add(new TreeMapNode<K, V>(key, value);
+	}
+	else
+	{
+		TreeMapNode<K, V> currentKey = BST->get(new TreeMapNode<K, V>(key));
+		currentKey.setValue(value);
+	}
 }
 
 template<class K, class V>
 int TreeMap<K, V>::size()
 {
-	return 0;
+	return BST->count();
 }
 
 template<class K, class V>
 bool TreeMap<K, V>::removeKey(K key)
 {
-	return false;
+	return BST->remove(new TreeMapNode<K, V>(key));
 }
 
 template<class K, class V>
-inline V& TreeMap<K, V>::operator[](K key)
+V& TreeMap<K, V>::operator[](K key)
 {
-	// TODO: insert return statement here
+	if (!containsKey(key))
+	{
+		throw logic_error("Key not found");
+	}
+	TreeMapNode<K, V> foundKey = BST->get(new TreeMapNode<K, V>(key));
+	return foundKey.getValue();
 }
 
 template <class K, class V>
 TreeMap<K, V>::~TreeMap()
 {
-
+	if (BST != nullptr) 
+	{
+		delete BST;
+		BST = nullptr;
+	}
 }

dff2ea4c584ee6532ade885e6bcdbfc83be35af1
Logan Rushe <D00255031@student.dkit.ie>
2024-11-22 10:59:51 +0000

    🚧 Started TreeMap Tests

---
+++ TreeMapTest/TreeMapTest.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CA2/TreeMap.h"
+#include "../CA2/TreeMapNode.h"
+#include "../CA2/BinaryTree.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TreeMapTest
+{
+	TEST_CLASS(TreeMapTest)
+	{
+	public:
+		
+		TEST_METHOD(TestClear)
+		{
+			TreeMap<int, string> *map = new TreeMap<int, string>();
+			string a = "a";
+			string b = "b";
+			string c = "c";
+			map->put(1, a);
+			map->put(2, b);
+			map->put(3, c);
+			Assert::AreEqual(map->get(1), a);
+			Assert::AreEqual(map->get(3), c);
+			map->clear();
+			string nullString = map->get(2);
+			Assert::IsNull(nullString);
+		}
+	};
+}
